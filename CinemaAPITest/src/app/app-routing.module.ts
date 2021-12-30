d340de0783f2490bbcbdae3414698c722e782342
Constantin ARMAND <constantin.armand@gmail.com>
2021-12-30 14:32:56 +0100

    detail

--- CinemaAPITest/src/app/app-routing.module.ts
+++ CinemaAPITest/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './page/detail/detail.component';
+import { PageUserComponent } from './page/page-user/page-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'search',component:PageUserComponent,pathMatch:'full'},
+  {path:'detail/:id/:type',component:DetailComponent,pathMatch:'full'},
+  {path: '**', redirectTo: 'search', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
