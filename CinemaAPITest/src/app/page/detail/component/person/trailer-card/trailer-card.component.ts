30b4b1047ebff36ac1583a043a37a13acc78d0dd
Constantin ARMAND <constantin.armand@gmail.com>
2022-01-03 12:13:43 +0100

    Trailer Card Fix

--- CinemaAPITest/src/app/page/detail/component/person/trailer-card/trailer-card.component.ts
+++ CinemaAPITest/src/app/page/detail/component/person/trailer-card/trailer-card.component.ts
@@ -10,11 +10,13 @@ export class TrailerCardComponent implements OnInit {
 
   constructor(private router : Router) { }
   @Input() data:any;
+  urlToLoad="";
   ngOnInit(): void {
+    this.urlToLoad="detail/"+this.data.id+"/"+this.data.media
   }
   ChangeUrl()
   {
-    this.router.navigate(['/detail',this.data.id,this.data.media]);
+    this.router.navigate(['/']).then(()=>{this.router.navigate(['/detail',this.data.id,this.data.media])});
   }
 
 }
