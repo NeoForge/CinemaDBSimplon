4bc6838350345d19e29cf2ceecc52fc2cb590996
ARMAND Constantin <constantin.armand@gmail.com>
2022-01-04 09:38:14 +0100

    After Merge fix in Rooting + Module

--- CinemaAPITest/src/app/app-routing.module.ts
+++ CinemaAPITest/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RandomComponent } from './components/random/random.component';
+import { AboutComponent } from './modules/about/about.component';
 import { DetailComponent } from './page/detail/detail.component';
 import { PageUserComponent } from './page/page-user/page-user.component';
 
 const routes: Routes = [
   {path:'search',component:PageUserComponent,pathMatch:'full'},
   {path:'detail/:id/:type',component:DetailComponent,pathMatch:'full'},
+  {path: 'random', component: RandomComponent},
+  {path: 'about', component: AboutComponent},
   {path: '', redirectTo: 'search', pathMatch: 'full'},
   {path: '**', redirectTo: 'search', pathMatch: 'full'},
 ];
-  {path: 'random', component: RandomComponent},
-  {path: 'about', component: AboutComponent},
+  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- CinemaAPITest/src/app/app.module.ts
+++ CinemaAPITest/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { PageUserComponent } from './page/page-user/page-user.component';
 import { FormsModule } from '@angular/forms';
 import { MovieCardComponent } from './components/movie-card/movie-card.component';
 import { ActorCardComponent } from './components/actor-card/actor-card.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { RandomComponent } from './components/random/random.component';
+import { NotfoundComponent } from './modules/notfound/notfound.component';
+import { HomeComponent } from './modules/home/home.component';
+import { AboutComponent } from './modules/about/about.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { DetailComponent } from './page/detail/detail.component';
 import { SafePipe } from './tools/safe.pipe';
 import { TvshowComponent } from './page/detail/component/tvshow/tvshow.component';
@@ -16,11 +20,6 @@ import { PersonComponent } from './page/detail/component/person/person.component
 import { MovieComponent } from './page/detail/component/movie/movie.component';
 import { TrailerCardComponent } from './page/detail/component/person/trailer-card/trailer-card.component';
 import { ActorCardDetailComponent } from './page/detail/component/other/actor-card-detail/actor-card-detail.component';
-import { RandomComponent } from './components/random/random.component';
-import { NotfoundComponent } from './modules/notfound/notfound.component';
-import { HomeComponent } from './modules/home/home.component';
-import { AboutComponent } from './modules/about/about.component';
-import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
@@ -28,15 +27,14 @@ import { MovieDetailComponent } from './components/movie-detail/movie-detail.com
     PageUserComponent,
     MovieCardComponent,
     ActorCardComponent,
-    NavbarComponent,
     DetailComponent,
     SafePipe,
     TvshowComponent,
     PersonComponent,
     MovieComponent,
     TrailerCardComponent,
-    ActorCardDetailComponent
-      RandomComponent,
+    ActorCardDetailComponent,
+    RandomComponent,
     NotfoundComponent,
     HomeComponent,
     AboutComponent,
