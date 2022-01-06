0ff88260c3c16a359ec63c3eb1849ba38415c672
Constantin ARMAND <constantin.armand@gmail.com>
2022-01-06 14:24:18 +0100

    Up PROD

--- CinemaAPITest/src/environments/environment.prod.ts
+++ CinemaAPITest/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey : "07dd7359e1dadfa94d57a40931b9bf32",
+  apiUrl: "https://api.themoviedb.org/3",
+  imageUrl: "https://image.tmdb.org/t/p/",
+  application:
+  {
+    name: 'api-simplon',
+    angular: 'Angular 13.1.1',
+    bootstrap: 'Bootstrap 5.1.3',
+    fontawesome: 'Font Awesome 5.15.4',
+  }
 };
